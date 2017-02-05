41286a1a5292c90f5700eac35e6b571c0b51f247
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2017-02-05 00:51:51 -0500

    1.0

---
+++ scraper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import csv
+import html5lib
+from urllib.request import urlopen
+import os
+import requests
+import csv
+
+class profile:
+    name = ''
+    link = ''
+
+# change the following variable for the address that you want to begin with
+# maybe your own FB profile
+
+initialUrl = "https://www.facebook.com/IgnacioGomezArboleda"
+
+def scrape(aUrl):
+    page = requests.get(aUrl).text
+    soup = BeautifulSoup(page, 'html5lib')
+    similarProfiles = soup.find_all('div', class_="profileFriendsText")
+    for aProfile in reversed(similarProfiles):
+        if aProfile.find_all('a')!=[]:
+            tag = aProfile.find('a')
+            newProfile = profile()
+            newProfile.name = aProfile.find('a').string
+            newProfile.link = tag.get('href').replace("https" , "http")
+            writeProfile(newProfile)
+            scrape(newProfile.link)
+
+def writeProfile(theProfile):
+    with open('results.csv','a') as csvfile:
+        fieldnames = ['name' , 'link']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({'name': theProfile.name , 'link': theProfile.link})
+
+scrape(initialUrl)
